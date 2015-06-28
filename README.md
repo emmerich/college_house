a4b13523ede453b76ac5b681c6e2f28e7283e9b1
Steven Hall <shne24@gmail.com>
2015-06-28 12:55:56 +1200

    Made application exiting cleaner

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-College House Management Application
+# College House Management Application
+
+## Setup
+* Install Nodejs
+* Clone the repository
+* Run ./install.sh
+* Create a configuration file (see below)
+* Run ./run.sh
+
+## Configuration File
+The only thing missing from the repository is a configuration file. Create a configuration file in the root direction and name is `configuration.json`. Here is a template:
+```javascript
+{
+	"channels": {
+		"Example Channel": {
+			"url": "https://mail.google.com",
+			"username": "myusername",
+			"password": "mypassword"
+		}
+	}
+}
+```
--- run.js
+++ run.js
@@ -3,6 +3,13 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var spawn = require('child_process').spawn;
+var client;
+var server;
+var openWindows = [];
+
+// Starting message
+console.log('College House Management Application starting up..');
 
 // The PORT that the web server will run on.
 var PORT = 8080;
@@ -16,13 +23,10 @@ var webdriverOpts = {
 		browserName: 'chrome',
 		chromeOptions: {
 			// Try to use the default profile
-			args: ['--user-data-dir=Library/Application Support/Google/Chrome/']
+			args: ['--user-data-dir=/Users/stvnhl/Library/Application Support/Google/Chrome/']
 		}
 	}
 };
-
-// Initialize the WebDriver client.
-var client = webdriverio.remote(webdriverOpts).init();
 
 // Set up Express to handle HTTP requests
 // Static will ensure any requests for files go to the file system.
@@ -44,26 +48,52 @@ app.post('/open_channels', function (req, res) {
 		var channelLookup = config.channels[channel];
 
 		// Open the channel in a new window
-		client.newWindow(channelLookup.url);
+		openWindows.push(client.newWindow(channelLookup.url));
 	});
 
 	res.status(200).end();
 });
 
-// Exit the application.
+// Exit the application
 app.get('/exit', function(req, res) {
-	client.end();
-	client.call(process.exit);
 	res.status(200).end();
+
+	console.log('Closing open windows..');
+	openWindows.forEach(function(w) {
+		w.close();
+	});
+
+	console.log('Closing browser..');
+	client.end();
+
+	client.call(function() {
+		console.log('Closing selenium-standalone..');
+		seleniumStandaloneProcess.kill('SIGINT');
+	});
+
+	setTimeout(function() {
+		console.log('Exiting application..');
+		process.exit(0);
+	}, 5000);
 });
+
+// Start the selenium-standalone as a child process
+console.log('Starting selenium-standalone server..');
+var seleniumStandaloneProcess = spawn('selenium-standalone', ['start']);
 
 // Run the HTTP server
-var server = app.listen(PORT, function () {
-	var host = server.address().address;
-	var port = server.address().port;
+setTimeout(function() {
+	console.log('Starting http server..');
+	server = app.listen(PORT, function () {
+		var host = server.address().address;
+		var port = server.address().port;
 
-	console.log('College House Management Application running on http://%s:%s', host, port);
+		console.log('College House Management Application running on http://localhost:%s', port);
 
-  	// Now open a new web browser and go to the management page.
-	client.url('http://localhost:' + PORT);
-});
+	  	// Initialize the WebDriver client.
+	  	client = webdriverio.remote(webdriverOpts).init();
+
+	  	// Now open a new web browser and go to the management page.
+		client.url('http://localhost:' + PORT);
+	});
+}, 5000);
